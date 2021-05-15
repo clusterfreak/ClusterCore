632a01e523ac4b3aa8fedc57b6960237387f55b1
Thomas Heym <thomas.heym@clusterfreak.de>
2021-05-15 18:50:23 +0200

    JavaDoc 16

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"de.clusterfreak.ClusterCore"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"de.clusterfreak.ClusterCore"}];updateSearchResults();
